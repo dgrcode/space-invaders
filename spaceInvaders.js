b49cbf4b4eb8b7b72e6fd6b3f948bf1c72c3f9b6
Daniel Gonzalez Reina <dangonrei@gmail.com>
2017-02-03 18:43:57 +0100

    added player shooting feature

--- spaceInvaders.js
+++ spaceInvaders.js
@@ -4,7 +4,6 @@
  	- Add blokcs to hide behind
  	- Make the game more difficult as time passes or as aliens get closer
  	- Make the aliens shoot
- 	- Make the player shoot
  	- Track the score
  	- Create a 3rd sprite for the aliens and insert it in the game
 */
@@ -27,10 +26,15 @@ function Game() {
 	let currentFrame = 0;
 	let aliens = []; // it has the array of aliens and two properties: rightMostAlein and leftMostAlien
 	let direction = 1; // start going right. Left would be -1
+	let waitFramesToMove = 10; // initial movements each 60 frames
 	let context = null;
 	let input = null;
 	let size = {width: null, height: null}; // size of the canvas. maybe better if game.size??
 	let player = null;
+	let playerBullet = null;
+	let lastBulletY = null;
+	let rightMostAlein = 0;
+	let leftMostAlien = 0;
 
 
 	/** Internal game functions **/
@@ -48,45 +52,92 @@ function Game() {
 
 	const tick = () => {
 		// update to the next game frame
-		console.log('-> ticks; direction: ' + direction);
+		//console.log('-> ticks; direction: ' + direction);
 		currentFrame++;
 
+		// End the game if user press 'Esc'
+		if (input.end) {game.end()}
+
 		// Game logic - updates
-			// if the pack of aliens reach the last 10% of the canvas (both sides),
-			// they jump down
-			if ((aliens.rightMost >= size.width * (1-moveLimit) && direction > 0) ||
-					(aliens.leftMost <= size.width * moveLimit && direction < 0)) {
-				for (let alien of aliens) {
-					alien.position.y += vJump;
 
-				}
-				// change direction
-				direction *= -1;
+			// check if an alien must die
 
-			} else if (currentFrame % waitFramesToMove == 0) {
+			aliens = aliens.filter(alien => {
+				return !(alien.hit && currentFrame - alien.frameHit >= framesDying);
+			});
+			
+
+			// updates the position each waitFramesToMove frames
+			if (currentFrame % waitFramesToMove == 0) {
 				// move all aliens horizontally
 				for (let alien of aliens) {
+					
 					// update horizontal position
 					alien.position.x += speed * direction;
 
-					// when going right, update leftMost & rightMost to the biggest.
-					// when going left update to the smallest 
-					if (direction > 0) {
-						if (alien.position.x + alien.size.width > aliens.rightMost) {
-							aliens.rightMost = alien.position.x + alien.size.width;
+					// upddate leftMostAlien & rightMostAlein
+						// when going right, update leftMostAlien & rightMostAlein to the biggest.
+						// When going left, update to the smallest.
+						// Comment: could be written shorten taking advantage of direction. less readable
+						if (direction > 0) {
+							if (alien.position.x + alien.size.width > rightMostAlein) {
+								rightMostAlein = alien.position.x + alien.size.width;
+							}
+							if (alien.position.x > leftMostAlien) {
+								leftMostAlien = alien.position.x;
+							}
+						} else {
+							if (alien.position.x + alien.size.width < rightMostAlein) {
+								rightMostAlein = alien.position.x + alien.size.width;
+							} 
+							if (alien.position.x < leftMostAlien) {
+								leftMostAlien = alien.position.x;
+							}
 						}
-						if (alien.position.x > aliens.leftMost) {
-							aliens.leftMost = alien.position.x;
-						}
-					} else {
-						if (alien.position.x + alien.size.width < aliens.rightMost) {
-							aliens.rightMost = alien.position.x + alien.size.width;
-						} 
-						if (alien.position.x < aliens.leftMost) {
-							aliens.leftMost = alien.position.x;
+					
+				}
+
+				// if the pack of aliens reach the last 10% of the canvas (both sides),
+				// they jump down
+				if ((rightMostAlein >= size.width * (1-moveLimit) && direction > 0) ||
+						(leftMostAlien <= size.width * moveLimit && direction < 0)) {
+					for (let alien of aliens) {
+						alien.position.y += vJump;
+
+					}
+					// change direction
+					direction *= -1;
+				}
+			}
+
+			// Move the bullet if there's one
+			if (playerBullet) {
+				// store last position to check hits during last step
+				lastBulletY = playerBullet.position.y;
+				
+				// update bullet's vertical position
+				playerBullet.position.y -= playerBullet.speed;
+
+				// check if the bullet has hit an alien
+				for (let alien of aliens) {
+					if (playerBullet &&
+							playerBullet.position.x + playerBullet.size.width >= alien.position.x &&
+							playerBullet.position.x <= alien.position.x + alien.size.width) {
+						// alien is at the same "column" than the bullet, now check for a
+						// collision using the y position
+						if (lastBulletY > alien.position.y + alien.size.height &&
+								playerBullet.position.y <= alien.position.y + alien.size.height) {
+							// In the last iteration the bullet crossed the alien
+							alien.hit = true;
+							alien.frameHit = currentFrame;
+							playerBullet = null;
 						}
 					}
-					
+				}
+
+				// make the bullet null if it goes out of the screen
+				if (playerBullet && playerBullet.position.y < 0) {
+					playerBullet = null;
 				}
 			}
 
@@ -98,8 +149,16 @@ function Game() {
 				player.position.x += 2;
 			}
 
+			// create a playerBullet if the space key is pressed and there is no
+			// other bullet
+			if (input.shoot && !playerBullet) {
+				playerBullet = PlayerBullet(
+						player.position.x + player.size.width / 2,
+						player.position.y);
+			}
+
 		// Render
-			fillStyle = "#000000";
+			context.fillStyle = "#000000";
 			context.fillRect(0, 0, size.width, size.height);
 			for (let alien of aliens) {
 				alien.render(context, currentFrame, waitFramesToMove);
@@ -107,6 +166,10 @@ function Game() {
 
 			if (player) {
 				player.render(context);
+			}
+
+			if (playerBullet) {
+				playerBullet.render(context);
 			}
 
 		// Call tick again
@@ -197,10 +260,18 @@ const Alien = (aX, aY) => {
 
 	alien.size = {width: 22, height: 16};
 
+	alien.hit = false;
+
+	alien.frameHit = null;
+
 	let img;
 	alien.render = (context, currentFrame, waitFramesToMove) => {
-		if (imgLoaded == 2) {
-			img = sprites[Math.floor(currentFrame/waitFramesToMove) % sprites.length];
+		if (imgLoaded == 3) {
+			if (alien.hit) {
+				img = imgHit;
+			} else {
+				img = sprites[Math.floor(currentFrame/waitFramesToMove) % sprites.length];
+			}
 
 			context.drawImage(img, alien.position.x, alien.position.y,
 					alien.size.width, alien.size.height);
@@ -238,7 +309,25 @@ const Player = (pX, pY) => {
 	}
 
 	return player;
-}
+};
+
+const PlayerBullet = (bX, bY) => {
+	const playerBullet = {};
+
+	playerBullet.position = {x: bX, y: bY};
+	
+	playerBullet.size = {width: 2, height: 4};
+
+	playerBullet.speed = 6;
+
+	playerBullet.render = (context) => {
+		context.fillStyle = "#ffffff";
+		context.fillRect(playerBullet.position.x, playerBullet.position.y,
+				playerBullet.size.width, playerBullet.size.height);
+	}
+
+	return playerBullet;
+};
 
 const UserInput = () => {
 	window.addEventListener('keydown', event => {
@@ -251,6 +340,10 @@ const UserInput = () => {
 			case 68: // 'd' or 'D'
 			case 39: // ArrowRight
 				state.right = true;
+				break;
+
+			case 32: // space bar
+				state.shoot = true;
 				break;
 
 			default:
@@ -270,6 +363,10 @@ const UserInput = () => {
 				state.right = false;
 				break;
 
+			case 32: // space bar
+				state.shoot = false;
+				break;
+
 			default:
 				// do nothing
 		}
@@ -278,10 +375,11 @@ const UserInput = () => {
 	const state = {
 		left: false,
 		right: false,
+		shoot: false,
 	}
 
 	return state;
-}
+};
 
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext('2d');
